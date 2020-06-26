6f208ea968d22e4fc1458f42f08973ba480feb55
Sam Cox <sam.cox@nottingham.ac.uk>
2020-06-26 14:38:25 +0100

    Add summary heatmap of all numeric columns' covariances. Includes the ability to add/remove columns.

--- src/app.py
+++ src/app.py
@@ -64,6 +64,22 @@ app.layout = html.Div(children=[
     html.H2(children="Table Summary"),
     html.Div(id='table_summary',
              style={'width': global_width}),
+    # html.Div(id='table_heatmap',
+    #          style={'width': global_width},
+    #          children=[dcc.Dropdown(id='table_heatmap_dropdown', style={'display': 'none'}),
+    #                    dcc.Graph(id='table_heatmap_fig',
+    #                              figure=go.Figure(go.Heatmap()),
+    #                              style={'display': 'none'}
+    #                              )
+    #                    ]
+    #          ),
+    html.Div(id='heatmap-div',
+             children=[html.Div(["Select variables to display:", dcc.Dropdown(id='heatmap-dropdown', options=([]))])]),
+    dcc.Graph(
+        id='heatmap',
+        figure=go.Figure(data=go.Heatmap())
+    ),
+
     # Hidden div for holding the jsonised combined DF in use
     html.Div(id='json-df-div', style={'display': 'none'}, children=[]),
 
@@ -416,28 +432,70 @@ def update_summary_table(input_json_df):
         dff.insert(loc=0, column='SUBJECTKEY(INDEX)', value=dff.index)
         description_df = dff.describe().transpose()
         description_df.insert(loc=0, column='', value=description_df.index)
-        return html.Div(dash_table.DataTable(
-            id='table',
-            columns=[{'name': i.upper(),
-                      'id': i,
-                      'type': 'numeric',
-                      'format': {'specifier': '.2f'}} for i in description_df.columns],
-            data=description_df.to_dict('record'),
-            # Highlight any columns that do not have a complete set of records,
-            # by comparing count against the length of the DF.
-            style_data_conditional=[
-                {
-                    'if': {
-                        'filter_query': '{{count}} < {}'.format(dff.shape[0]),
-                        'column_id': 'count'
-                    },
-                    'backgroundColor': 'FireBrick',
-                    'color': 'white'
-                }
-            ]
-        ),
+        return html.Div([html.Div(['nrows:' + str(dff.shape[0]),
+                                   'ncols:' + str(dff.shape[1])]
+                                  ),
+                         html.Div(
+                             dash_table.DataTable(
+                                 id='table',
+                                 columns=[{'name': i.upper(),
+                                           'id': i,
+                                           'type': 'numeric',
+                                           'format': {'specifier': '.2f'}} for i in description_df.columns],
+                                 data=description_df.to_dict('record'),
+                                 # Highlight any columns that do not have a complete set of records,
+                                 # by comparing count against the length of the DF.
+                                 style_data_conditional=[
+                                     {
+                                         'if': {
+                                             'filter_query': '{{count}} < {}'.format(dff.shape[0]),
+                                             'column_id': 'count'
+                                         },
+                                         'backgroundColor': 'FireBrick',
+                                         'color': 'white'
+                                     }
+                                 ]
+                             )
+                         )
+                         ]
                         )
+
     return html.Div()
+
+
+@app.callback(
+    Output('heatmap-div', 'children'),
+    [Input('json-df-div', 'children')]
+)
+def update_heatmap_dropdown(input_json_df):
+    dff = pd.read_json(json.loads(input_json_df), orient='split')
+    options = [{'label': col,
+                'value': col} for col in dff.columns if dff[col].dtype in [np.int64, np.float64]]
+    return [html.Div(["Select variables to display:", dcc.Dropdown(id='heatmap-dropdown',
+                                                                   options=options,
+                                                                   value=list(dff.columns),
+                                                                   multi=True)])]
+
+
+@app.callback(
+    Output('heatmap', 'figure'),
+    [Input('json-df-div', 'children'),
+     Input('heatmap-dropdown', 'value')])
+def update_summary_heatmap(input_json_df, dropdown_values):
+    print('update_summary_heatmap')
+    dff = pd.read_json(json.loads(input_json_df), orient='split')
+    # Add the index back in as a column so we can see it in the table preview
+    if dff.size > 0:
+        dff.insert(loc=0, column='SUBJECTKEY(INDEX)', value=dff.index)
+        selected_columns = list(dropdown_values)
+        cov = dff[selected_columns].cov()
+        return go.Figure(go.Heatmap(z=cov,
+                                    x=cov.columns,
+                                    y=cov.columns
+                                    )
+                         )
+
+    return go.Figure(go.Heatmap())
 
 
 @app.callback(
