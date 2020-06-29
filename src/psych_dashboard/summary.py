c920a64f00264fa366b9cfbcaf366566b9d8676f
Sam Cox <sam.cox@nottingham.ac.uk>
2020-06-29 17:20:26 +0100

    Use json as State argument to update_summary_heatmap() (to avoid issues with the order of calling of callbacks). Rename cov ->corr, w_cov -> w_corr etc.

--- src/psych_dashboard/summary.py
+++ src/psych_dashboard/summary.py
@@ -5,7 +5,7 @@ import numpy as np
 import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.graph_objects as go
 from psych_dashboard.app import app
 from scipy.cluster.vq import kmeans, vq, whiten
@@ -72,41 +72,45 @@ def update_heatmap_dropdown(input_json_df):
 
 @app.callback(
     Output('heatmap', 'figure'),
-    [Input('json-df-div', 'children'),
-     Input('heatmap-dropdown', 'value')])
-def update_summary_heatmap(input_json_df, dropdown_values):
+    [Input('heatmap-dropdown', 'value')],
+    [State('json-df-div', 'children')])
+def update_summary_heatmap(dropdown_values, input_json_df):
     print('update_summary_heatmap')
-    dff = pd.read_json(json.loads(input_json_df), orient='split')
-    # Add the index back in as a column so we can see it in the table preview
-    if dff.size > 0 and dropdown_values != []:
-        dff.insert(loc=0, column='SUBJECTKEY(INDEX)', value=dff.index)
-        selected_columns = list(dropdown_values)
-        print('selected_columns', selected_columns)
-        cov = dff[selected_columns].corr()
-        print('cov', cov)
-        # K-means clustering of covariance values.
-        w_cov = whiten(cov)
-        print(w_cov)
-        centroids, _ = kmeans(w_cov, min(7,int(math.sqrt(w_cov.shape[0]))))
-        print(centroids)
-        # Generate the indices for each column, which cluster they belong to.
-        clx, _ = vq(w_cov, centroids)
-        print(clx)
-        print([x for _,x in sorted(zip(clx,selected_columns))])
-        # TODO: what would be good here would be to rename the clusters based on the average variance (diags) within
-        # TODO: each cluster - that would reduce the undesirable behaviour whereby currently the clusters can jump about
-        # TODO: when re-calculating the clustering.
-        # Sort cov columns
-        half_sorted_cov = cov[[x for _,x in sorted(zip(clx,selected_columns))]]
-        print(half_sorted_cov)
-        # Sort cov rows
-        sorted_cov = half_sorted_cov.reindex([x for _, x in sorted(zip(clx, selected_columns))])
-        return go.Figure(go.Heatmap(z=sorted_cov,
-                                    x=sorted_cov.columns,
-                                    y=sorted_cov.columns,
-                                    zmin=-1,
-                                    zmax=1,
-                                    colorscale='RdBu')
-                         )
+    print('i', input_json_df)
+    # Guard against the second argument being an empty list, as happens at first invocation
+    if not input_json_df == []:
+        dff = pd.read_json(json.loads(input_json_df), orient='split')
+        # Add the index back in as a column so we can see it in the table preview
+        if dff.size > 0 and dropdown_values != []:
+            dff.insert(loc=0, column='SUBJECTKEY(INDEX)', value=dff.index)
+            selected_columns = list(dropdown_values)
+            print('selected_columns', selected_columns)
+            corr = dff[selected_columns].corr(min_periods=1)
+            print('corr', corr)
+            # K-means clustering of correlation values.
+            corr.fillna(0, inplace=True)
+            w_corr = whiten(corr)
+            print(w_corr)
+            centroids, _ = kmeans(w_corr, min(7, int(math.sqrt(w_corr.shape[0]))))
+            print(centroids)
+            # Generate the indices for each column, which cluster they belong to.
+            clx, _ = vq(w_corr, centroids)
+            print(clx)
+            print([x for _,x in sorted(zip(clx, selected_columns))])
+            # TODO: what would be good here would be to rename the clusters based on the average variance (diags) within
+            # TODO: each cluster - that would reduce the undesirable behaviour whereby currently the clusters can jump about
+            # TODO: when re-calculating the clustering.
+            # Sort corr columns
+            half_sorted_corr = corr[[x for _, x in sorted(zip(clx, selected_columns))]]
+            print(half_sorted_corr)
+            # Sort corr rows
+            sorted_corr = half_sorted_corr.reindex([x for _, x in sorted(zip(clx, selected_columns))])
+            return go.Figure(go.Heatmap(z=sorted_corr,
+                                        x=sorted_corr.columns,
+                                        y=sorted_corr.columns,
+                                        zmin=-1,
+                                        zmax=1,
+                                        colorscale='RdBu')
+                             )
 
     return go.Figure(go.Heatmap())
