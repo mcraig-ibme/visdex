55dc3704ff84be2af83816e88971af3fd0769d19
Sam Cox <sam.cox@nottingham.ac.uk>
2020-07-21 12:33:18 +0100

    Add filtering functionality based upon % of missing values per column. Columns that fail this threshold are not available for further use. This makes use of a second feather file 'df_filtered.feather' to hold the filtered DF.

--- src/psych_dashboard/load_feather.py
+++ src/psych_dashboard/load_feather.py
@@ -12,3 +12,15 @@ def load_feather(df_loaded):
     if df_loaded and len(dff) > 0:
         dff.set_index(indices, inplace=True)
     return dff
+
+
+def load_filtered_feather(df_loaded):
+    """
+    Utility function for the common task of reading DF from feather file, and setting the MultiIndex. This is called
+    every time the main DF, filtered by missing values, needs to be accessed.
+    """
+    dff = pd.read_feather('df_filtered.feather')
+
+    if df_loaded and len(dff) > 0:
+        dff.set_index(indices, inplace=True)
+    return dff
