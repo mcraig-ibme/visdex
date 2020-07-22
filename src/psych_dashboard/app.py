7f99d51f9c4595f86f613e2604c66d8f98e94c00
Sam Cox <sam.cox@nottingham.ac.uk>
2020-07-22 17:33:01 +0100

    Add DBC Collapse functionality around summary section and exploratory section.

--- src/psych_dashboard/app.py
+++ src/psych_dashboard/app.py
@@ -1,6 +1,7 @@
 import dash
+import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__, suppress_callback_exceptions=False)
+app = dash.Dash(__name__, suppress_callback_exceptions=False, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
 indices = ['SUBJECTKEY', 'EVENTNAME']
