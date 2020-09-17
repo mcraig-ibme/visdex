6539d30320d4e8faf9565cfd5485c16c8f208b99
Sam Cox <sam.cox@nottingham.ac.uk>
2020-09-17 11:10:53 +0100

    Move dependency information into setup.py, and remove requirements.txt.

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
 from setuptools import setup, find_packages
 
-setup(name="psych_dashboard", version='0.0.1', packages=find_packages(where='src'), package_dir={"": "src"})
+setup(name="psych_dashboard",
+      version='0.0.1',
+      description='A package to run a Plotly Dash dashboard to illustrate data from single data files',
+      install_requires=['pandas>=1.0.0',
+                        'xlrd>=1.2.0',
+                        'plotly>=4.8.0',
+                        'dash>=1.12.0',
+                        'scikit-learn>=0.22.0',
+                        'numpy>=1.16.0',
+                        'scipy>=1.2.0',
+                        'feather-format>=0.4.0',
+                        'dash-bootstrap-components>=0.10.0',
+                        ],
+      packages=find_packages(where='src'),
+      package_dir={"": "src"},
+      )
