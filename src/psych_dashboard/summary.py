23c4413eb54b967a751f3a31f4e15dbdff3453da
Sam Cox <sam.cox@nottingham.ac.uk>
2020-09-22 17:18:26 +0100

    We now only set necessary elements of logs. This commit removes code that set _all_ the logs, and that then passed over them removing the unnecessary elements. Saves 1/3 of correlation compute time.

--- src/psych_dashboard/summary.py
+++ src/psych_dashboard/summary.py
@@ -198,6 +198,11 @@ def recalculate_corr_etc(selected_columns, dff, corr_dff, pval_dff, logs_dff):
 
     ts1 = time.time()
     counter = 0
+
+    # Explicitly query this here as I can't fully guarantee the relationship between the ordering of this
+    # and the ordering of selected_columns or overlap + required_new
+    logs_columns = logs.columns
+
     for v1, v2 in product(selected_columns, required_new):
         # Use counter to work out the indexing into the pre-numpyed arrays.
         v2_counter = counter % len(required_new)
@@ -205,27 +210,20 @@ def recalculate_corr_etc(selected_columns, dff, corr_dff, pval_dff, logs_dff):
         # Calculate corr and p-val
         if pd.isna(corr.at[v1, v2]):
             corr.at[v1, v2], pvalues.at[v1, v2] = stats.pearsonr(np_dff_sel[:, v1_counter], np_dff_req[:, v2_counter])
-            logs.at[v1, v2] = -np.log10(pvalues.at[v1, v2])
+            # Only populate the upper triangle (exc diagonal) of the logs DF.
+            if v1 != v2:
+                if logs_columns.to_list().index(v1) < logs_columns.to_list().index(v2):
+                    logs.at[v1, v2] = -np.log10(pvalues.at[v1, v2])
+                else:
+                    logs.at[v2, v1] = -np.log10(pvalues.at[v1, v2])
             # Populate the other half of the matrix
             corr.at[v2, v1] = corr.at[v1, v2]
             pvalues.at[v2, v1] = pvalues.at[v1, v2]
-            logs.at[v2, v1] = logs.at[v1, v2]
+
         counter += 1
 
     te1 = time.time()
-    timing_dict['update_summary_heatmap-calc'] = te1 - ts1   # 29 of 43s total.
-
-    # ts1 = time.time()
-
-    # Now blank out any duplicates in logs including the diagonal. While this may seem
-    # wasteful, to calculate them all and then delete some, it's the cleanest way to get the triangular matrix
-    for ind in logs.index:
-        for col in logs.columns:
-            if ind in logs.columns and col in logs.index and logs[col][ind] == logs[ind][col]:
-                logs[ind][col] = np.nan
-
-    te1 = time.time()
-    timing_dict['update_summary_heatmap-nan'] = te1 - ts1  # 14 of 43s total
+    timing_dict['update_summary_heatmap-calc'] = te1 - ts1   # 29 of 43s total. Then 28 of 28 once nan removal is ditched :D
 
     te = time.time()
     timing_dict['update_summary_heatmap-corr'] = te - ts
