73fa2ac83a5b9cfc7b83b3a5ba02b03848c8342c
Sam Cox <sam.cox@nottingham.ac.uk>
2020-09-28 14:17:01 +0100

    Fix timing function bug introduced in a0d72fb

--- src/psych_dashboard/summary.py
+++ src/psych_dashboard/summary.py
@@ -28,7 +28,7 @@ def timing(f):
         ts = time.time()
         result = f(*args, **kw)
         te = time.time()
-        logging.info(f'#### func:{f.__name__} took: {te-te:.2f} sec')
+        logging.info(f'#### func:{f.__name__} took: {te-ts:.2f} sec')
         timing_dict[f.__name__] = te-ts
         return result
     return wrap
