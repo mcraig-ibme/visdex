d121f1bfa660b67b9f01eae62029eb168e2b90ea
Martin Craig <martin.craig@eng.ox.ac.uk>
2021-06-23 18:53:25 +0100

    Add versioning in setup script, license and bundling of assets files

--- setup.py
+++ setup.py
@@ -1,23 +1,114 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+"""
+Setup script for vaby
+"""
+import os
+import subprocess
+import re
+import io
+import glob
 
-setup(name="visdex",
-      version='0.0.1',
-      description='A package to run a Plotly Dash dashboard to illustrate data from single data files',
-      install_requires=['pandas>=1.0.0',
-                        'xlrd>=1.2.0',
-                        'plotly>=4.8.0',
-                        'dash>=1.12.0',
-                        'scikit-learn>=0.22.0',
-                        'numpy>=1.16.0',
-                        'scipy>=1.2.0',
-                        'feather-format>=0.4.0',
-                        'dash-bootstrap-components>=0.10.0',
-                        'selenium>=3.0.0',
-                        'reportlab>=3.5.50',
-                        'flask-caching>=1.9.0',
-                        ],
-      entry_points={
-         'console_scripts': ['run-visdex=visdex.index:main'],
-        },
-      packages=find_packages(),
-      )
+from setuptools import setup
+from setuptools import find_packages
+
+MODULE = 'visdex'
+
+def get_filetext(rootdir, filename):
+    """ Get the text of a local file """
+    with io.open(os.path.join(rootdir, filename), encoding='utf-8') as f_handle:
+        return f_handle.read()
+
+def git_version():
+    """ Get the full and python standardized version from Git tags (if possible) """
+    try:
+        # Full version includes the Git commit hash
+        full_version = subprocess.check_output('git describe --dirty', shell=True).decode("utf-8").strip(" \n")
+
+        # Python standardized version in form major.minor.patch.post<build>
+        version_regex = re.compile(r"v?(\d+\.\d+\.\d+(-\d+)?).*")
+        match = version_regex.match(full_version)
+        if match:
+            std_version = match.group(1).replace("-", ".post")
+        else:
+            raise RuntimeError("Failed to parse version string %s" % full_version)
+        return full_version, std_version
+    except:
+        # Any failure, return None. We may not be in a Git repo at all
+        return None, None
+
+def git_timestamp():
+    """ Get the last commit timestamp from Git (if possible)"""
+    try:
+        return subprocess.check_output('git log -1 --format=%cd', shell=True).decode("utf-8").strip(" \n")
+    except:
+        # Any failure, return None. We may not be in a Git repo at all
+        return None
+
+def update_metadata(rootdir, version_str, timestamp_str):
+    """ Update the version and timestamp metadata in the module _version.py file """
+    with io.open(os.path.join(rootdir, MODULE, "_version.py"), "w", encoding='utf-8') as f_handle:
+        f_handle.write("__version__ = '%s'\n" % version_str)
+        f_handle.write("__timestamp__ = '%s'\n" % timestamp_str)
+
+def get_requirements(rootdir):
+    """ Get a list of all entries in the requirements file """
+    with io.open(os.path.join(rootdir, 'requirements.txt'), encoding='utf-8') as f_handle:
+        return [l.strip() for l in f_handle.readlines()]
+
+def get_version(rootdir):
+    """ Get the current version number (and update it in the module _version.py file if necessary)"""
+    version, timestamp = git_version()[1], git_timestamp()
+
+    if version is not None and timestamp is not None:
+        # We got the metadata from Git - update the version file
+        update_metadata(rootdir, version, timestamp)
+    else:
+        # Could not get metadata from Git - use the version file if it exists
+        try:
+            with io.open(os.path.join(rootdir, MODULE, '_version.py'), encoding='utf-8') as f_handle:
+                metadata = f_handle.read()
+                match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", metadata, re.M)
+                if match:
+                    version = match.group(1)
+                else:
+                    version = "unknown"
+        except IOError:
+            version = "unknown"
+
+    return version
+
+def get_package_data(rootdir):
+    """
+    Get extra data files to install into the package, e.g. icons
+    """
+    return {
+        MODULE : glob.glob("%s/*.png" % rootdir) + glob.glob("%s/*.svg" % rootdir) + glob.glob("%s/*.nii.gz" % rootdir) + glob.glob("%s/*.css" % rootdir)
+    }
+
+MODULE_DIR = os.path.abspath(os.path.dirname(__file__))
+KWARGS = {
+    'name' : 'visdex',
+    'version' : get_version(MODULE_DIR),
+    'description' : 'A Plotly Dash dashboard to visualise data from tabular data files',
+    'long_description' : get_filetext(MODULE_DIR, 'README.md'),
+    'long_description_content_type' : 'text/markdown',
+    'author' : 'Sam Cox, Matteo Battestini, Martin Craig',
+    'author_email' : 'martin.craig@eng.ox.ac.uk',
+    'license' : 'Apache 2',
+    'install_requires' : get_requirements(MODULE_DIR),
+    'packages' : find_packages(),
+    'package_data' : get_package_data(os.path.join(MODULE_DIR, MODULE, "assets")),
+    'entry_points' : {
+         'console_scripts': ['run-visdex=visdex.__main__:main'],
+    },
+    'classifiers' : [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+}
+
+print(KWARGS["package_data"])
+setup(**KWARGS)
--- visdex/__main__.py
+++ visdex/__main__.py
@@ -2,6 +2,9 @@ import logging
 
 from visdex.app import app
 
-# Default logging to stdout
-logging.basicConfig(level=logging.INFO)
-app.run_server(debug=False)
+def main():
+    # Default logging to stdout
+    logging.basicConfig(level=logging.INFO)
+    app.run_server(debug=False)
+
+main()
