1dc164c1666b78e69e0e3d514b5e9b8d95da4018
Martin Craig <martin.craig@nottingham.ac.uk>
2022-01-13 12:15:40 +0000

    Enable LDAP login

--- visdex/login.py
+++ visdex/login.py
@@ -1,12 +1,21 @@
+import logging
+
 from flask_login import LoginManager, UserMixin, login_user, current_user
 
 from dash import html, dcc
 from dash.dependencies import Input, Output, State
 
+from ldap3 import Server, Connection, ALL
+import ldap
+
+LDAP_SERVER="ldaps://uonauth.nottingham.ac.uk/"
+
 class User(UserMixin):
     pass
 
-USERS = {'bbzmsc': {'password': 'secret'}}
+USERS = {'bbzmsc': {}}
+
+LOG = logging.getLogger(__name__)
 
 def get_user(userid):
     if userid in USERS:
@@ -15,9 +24,55 @@ def get_user(userid):
         return user
  
 def check_password(userid, password):
-    user = get_user(userid)
-    if user and password == USERS[userid]["password"]:
-        return user
+    user = get_user(userid)    
+    if user:
+        try:
+            ldap_client = ldap.initialize(LDAP_SERVER)
+            # perform a synchronous bind
+            ldap_client.set_option(ldap.OPT_REFERRALS, 0)
+            
+            dns = ldap_client.search_st("ou=accounts,o=university", ldap.SCOPE_SUBTREE, filterstr='(uid=%s)' % userid, timeout=-1)
+            if not dns:
+                LOG.warn("No matching user found: %s" % userid)
+                return
+
+            dn = dns[0][0]
+            if len(dns) > 1:
+                LOG.warn("Multiple matching DNs for user ID")
+                for dn, _attrs in dns:
+                    LOG.warn(" - %s" % dn)
+            else:
+                LOG.info("%s->%s" % (userid, dn))
+            ldap_client.simple_bind_s(dn, password)
+            print("Authentication successful")
+            return user
+        except ldap.INVALID_CREDENTIALS:
+            print('Authentication failed')
+        except ldap.SERVER_DOWN:
+            print('AD server not available')
+        finally:
+            ldap_client.unbind()
+
+        # server = Server(LDAP_SERVER, use_ssl=True, get_info=ALL)
+        # LOG.info("Server " + str(server))
+        # ldap_username = 'cn=%s,ou=Staff,ou=Active,ou=Accounts,o=University' % userid
+        # conn = Connection(server, ldap_username, password, auto_bind=False, raise_exceptions=False)
+        # LOG.info("Conn" + str(conn))
+        # try:
+        #     LOG.info("Binding")
+        #     conn.bind()
+        #     LOG.info("Bound: %s" % conn.result)
+        #     if conn.result['result'] == 0:  # Successful
+        #         return user
+        # except Exception as e:
+        #     LOG.info("Exc ")
+        #     LOG.warn(str(e))
+        # except:
+        #     LOG.info("???")
+        # finally:
+        #     LOG.info("Unbinding")
+        #     if conn.bound:
+        #         conn.unbind()
 
 def init_login(flask_app):
     login_manager = LoginManager()
