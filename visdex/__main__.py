443291c7a7d561e6238fdc6ae9bad03c3ee7ba9b
Martin Craig <martin.craig@nottingham.ac.uk>
2022-01-14 12:28:48 +0000

    Make sure logging is available when app imported

--- visdex/__main__.py
+++ visdex/__main__.py
@@ -1,10 +1,9 @@
-import logging
-
-from visdex.app import app
-
 def main():
     # Default logging to stdout
+    import logging
     logging.basicConfig(level=logging.INFO)
+
+    from visdex.app import app
     app.run_server(debug=False)
 
 main()
