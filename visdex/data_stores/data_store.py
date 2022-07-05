c2eda6254c137d8fbe14e817f00e7d15b40ee97b
Martin Craig <martin.craig@nottingham.ac.uk>
2022-07-05 13:49:21 +0100

    Add method to check user can access a data store

--- visdex/data_stores/data_store.py
+++ visdex/data_stores/data_store.py
@@ -3,10 +3,17 @@ visdex: Base class for data stores
 """
 import logging
 
+from flask_login import current_user
+
 class DataStore:
     """
     A data store is a stateless object which provides access to a particular type of data
     """
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.log = logging.getLogger(__name__)
+        self.users = kwargs.get("users", None)
+
+    def check_user(self):
+        self.log.info("Checking user %s vs %s" % (current_user.get_id(), self.users))
+        return self.users is None or (current_user.is_authenticated and current_user.get_id() in self.users)
