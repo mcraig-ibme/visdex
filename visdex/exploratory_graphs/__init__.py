6c37f507f04d2fa4dff1b45f2d5c5f20015d7c0e
Martin Craig <martin.craig@nottingham.ac.uk>
2022-09-27 16:06:28 +0100

    Basic refactoring of exploratory graphs

--- visdex/exploratory_graphs/__init__.py
+++ visdex/exploratory_graphs/__init__.py
@@ -10,14 +10,8 @@ import plotly.graph_objects as go
 
 from visdex.common import Collapsible
 
-from .common import all_components
-from . import (
-    bar_graph,
-    histogram_graph,
-    manhattan_graph,
-    scatter_graph,
-    violin_graph,
-)
+from .common import all_components, add_graph_type
+from . import bar, histogram, manhattan, scatter, violin
 
 class ExploratoryGraphs(Collapsible):
     def __init__(self, app, id_prefix="exp-"):
@@ -50,11 +44,11 @@ class ExploratoryGraphs(Collapsible):
             ],
         )
             
-        bar_graph.define_cbs(app)
-        histogram_graph.define_cbs(app)
-        manhattan_graph.define_cbs(app)
-        scatter_graph.define_cbs(app)
-        violin_graph.define_cbs(app)
+        add_graph_type(app, "bar", bar.make_figure)
+        add_graph_type(app, "histogram", histogram.make_figure)
+        add_graph_type(app, "manhattan", manhattan.make_figure)
+        add_graph_type(app, "scatter", scatter.make_figure)
+        add_graph_type(app, "violin", violin.make_figure)
         
     def add_graph(self, n_clicks, children):
         # Add a new graph group each time the button is clicked. The if None guard stops
--- visdex/exploratory_graphs/bar.py
+++ visdex/exploratory_graphs/bar.py
@@ -4,15 +4,9 @@ visdex: Bar graph
 import logging
 import plotly.graph_objects as go
 
-import visdex.session
-from .common import common_define_cbs
-
 LOG = logging.getLogger(__name__)
 
-def define_cbs(app):
-    common_define_cbs(app, "bar", make_bar_figure)
-
-def make_bar_figure(dff, args_dict):
+def make_figure(dff, args_dict):
     # Return empty scatter if not enough options are selected, or the data is empty.
     if dff.columns.size == 0 or args_dict["x"] is None:
         return go.Figure(go.Bar())
